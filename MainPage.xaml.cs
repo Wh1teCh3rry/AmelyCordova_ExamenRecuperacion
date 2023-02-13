b681e0f6063db45683ccc0a5165d13237e13b8a7
Amely Cordova <amely.cordova@udla.edu.ec>
2023-02-13 09:41:15 -0500

    Creación de interfaz en MainPage

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace AmelyCordova_ExamenRecuperacion;
+﻿using Newtonsoft.Json;
+using System.Net;
+using AmelyCordova_ExamenRecuperacion.ACModels;
+
+namespace AmelyCordova_ExamenRecuperacion;
 
 public partial class MainPage : ContentPage
 {
@@ -9,16 +13,18 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    public async void Button_Clicked(object sender, EventArgs e)
+    {
+        var request = new HttpRequestMessage();
+        request.RequestUri = new Uri("https://dog.ceo/api/breed/Pomeranian/images/random");
+        request.Method = HttpMethod.Get;
+        request.Headers.Add("Accept", "application/json"); var client = new HttpClient(); HttpResponseMessage response = await client.SendAsync(request);
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            String content = await response.Content.ReadAsStringAsync();
+            var resultado = JsonConvert.DeserializeObject<List<Root>>(content);
+            ListaDog.ItemsSource = resultado;
+        }
+    }
 }
 
