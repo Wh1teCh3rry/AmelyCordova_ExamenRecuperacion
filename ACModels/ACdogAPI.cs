660068e1d7f09d301cb559b99d23c2ffb4724459
Amely Cordova <amely.cordova@udla.edu.ec>
2023-02-13 09:30:38 -0500

    Creación de Models

---
+++ ACModels/ACdogAPI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmelyCordova_ExamenRecuperacion.ACModels
+{
+    public class Message
+    {
+        public List<object> pomeranian { get; set; }
+    }
+
+    public class Root
+    {
+        public string status { get; set; }
+        public Message message { get; set; }
+    }
+}
