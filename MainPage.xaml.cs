5ddb88205421ddb75bbeaf54476552e5eb24c757
Amely Cordova <amely.cordova@udla.edu.ec>
2023-02-13 10:48:28 -0500

    Muestra imágenes en interfaz Boton llamar API funcionando

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,16 +6,14 @@ namespace AmelyCordova_ExamenRecuperacion;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
     public async void ACButton_Clicked(object sender, EventArgs e)
     {
-        WebRequest request1 = WebRequest.Create("https://dog.ceo/api/breed/Pomeranian/images/random/10");
+        WebRequest request1 = WebRequest.Create("https://dog.ceo/api/breed/pomeranian/images");
         request1.Headers.Add("Accept", "application/json");
         WebResponse response1 = request1.GetResponse();
         using (Stream dataStream = response1.GetResponseStream())
@@ -24,17 +22,26 @@ public partial class MainPage : ContentPage
             string responseFromServer1 = reader1.ReadToEnd();
             responseFromServer1 = responseFromServer1.Trim();
             var resultado1 = JsonConvert.DeserializeObject<Root>(responseFromServer1);
-            string imagen = resultado1.status;
-            ACimage1.Source = imagen;
-            ACimage2.Source = imagen;
-            ACimage3.Source = imagen;
-            ACimage4.Source = imagen;
-            ACimage5.Source = imagen;
-            ACimage6.Source = imagen;
-            ACimage7.Source = imagen;
-            ACimage8.Source = imagen;
-            ACimage9.Source = imagen;
-            ACimage10.Source = imagen;
+            string imagen1 = resultado1.message[0];
+            string imagen2 = resultado1.message[1];
+            string imagen3 = resultado1.message[2];
+            string imagen4 = resultado1.message[3];
+            string imagen5 = resultado1.message[4];
+            string imagen6 = resultado1.message[5];
+            string imagen7 = resultado1.message[6];
+            string imagen8 = resultado1.message[7];
+            string imagen9 = resultado1.message[8];
+            string imagen10 = resultado1.message[9];
+            ACimage1.Source = imagen1;
+            ACimage2.Source = imagen2;
+            ACimage3.Source = imagen3;
+            ACimage4.Source = imagen4;
+            ACimage5.Source = imagen5;
+            ACimage6.Source = imagen6;
+            ACimage7.Source = imagen7;
+            ACimage8.Source = imagen8;
+            ACimage9.Source = imagen9;
+            ACimage10.Source = imagen10;
         }
     }
 }
